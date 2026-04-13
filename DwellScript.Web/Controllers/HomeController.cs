3295792f8f9b460eff90c6ec7d24d65d62388435
sanjeevhavanur <sanjeevhavanur@gmail.com>
2026-04-13 16:48:05 -0400

    Add Pricing page and update links; adjust pricing details in the index

--- DwellScript.Web/Controllers/HomeController.cs
+++ DwellScript.Web/Controllers/HomeController.cs
@@ -30,6 +30,12 @@ public class HomeController : Controller
         return View();
     }
 
+    [Route("Pricing")]
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
